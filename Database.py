2c9e4e6332aee8bc0fa35d02c9408606b4d62de0
AviAbhi716 <128021406+AviAbhi716@users.noreply.github.com>
2025-03-19 00:08:22 +0530

    Initial Commit

---
+++ Database.py
@@ -0,0 +1,14 @@
+questions={
+    'Who is known as the "God of Cricket"?':"C",
+    "Which country won the ICC Cricket World Cup in 2019?":"C",
+    "What is the maximum number of overs in a One Day International (ODI) cricket match?":"B",
+    "Which cricketer holds the record for the highest individual score in a single Test innings?":"B",
+    "In cricket, what does LBW stand for?":"B"
+}
+options=[
+    "A) Ricky Ponting \nB) Brian Lara \nC) Sachin Tendulkar \nD) Virat Kohli",
+    "A) New Zealand \nB) Australia \nC) England \nD) India",
+    "A) 20 \nB) 50 \nC) 100 \nD) 60",
+    "A) Don Bradman \nB) Brian Lara \nC) Sachin Tendulkar \nD) Steve Smith",
+    "A) Long Ball Win \nB) Leg Before Wicket \nC) Last Ball Win \nD) Left Behind Wicket"
+]
